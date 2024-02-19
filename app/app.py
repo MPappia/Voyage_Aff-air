1256e0ab14a28c90a9e219ed8224aab4d4912809
Pr. Pappia <102623712+MPappia@users.noreply.github.com>
2024-02-19 19:02:39 +0100

    Delete app directory

--- app/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-if __name__ == "__main__":
-    app.run(app.config["DEBUG"])
--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-import dotenv
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-dotenv.load_dotenv(os.path.join(BASE_DIR, '.env'))
-
-class Config(): 
-    DEBUG = os.environ.get('DEBUG')
