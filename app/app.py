dc72e5a1203616c5fa0429521f0bcd9cccefee93
MPappia <elliot.fabert@gmail.com>
2024-02-19 15:10:20 +0100

    Bidouille et Compagnie

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
+from config import Config
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 
 if __name__ == "__main__":
     app.run(debug=True)
