0b2929f91e8f0a52387790facf3d8e60b2d87b6d
MPappia <elliot.fabert@gmail.com>
2024-02-19 12:40:05 +0100

    suppression de la viz de test et changement des routes pour les pages de test.

--- app/app.py
+++ app/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
-from routes.generales import generales_bp
 
 app = Flask(__name__)
 
-app.register_blueprint(generales_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
