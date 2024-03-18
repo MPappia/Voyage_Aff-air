b281448a5fa42f23fdf9de6e0dc3904306d5b9e8
Laitauchocolat34 <146727591+Laitauchocolat34@users.noreply.github.com>
2024-03-18 10:57:49 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,15 @@
 from ..app import app, db
+from Datetime import datetime
 
 class person(db.Model):
     __tablename__ = "Person"
+    
+    fonction = db.Column
+    nom = db.Column
+    nationalite_code = db.Column
+    Date_debut_mandat = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
+    Date_fin = db.Column(db.DateTime, default=datetime.utcnow, nullable=True)
+    parti = db.Column(db.String(45, nullable=True)
 
 
 
