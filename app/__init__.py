968b13d677e65dd17da93e27327d9f2ac9f9c4fd
MPappia <elliot.fabert@gmail.com>
2024-03-19 11:48:41 +0100

    connexion à la base de données

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config.from_object(Config)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data', 'projet.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/projet.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
