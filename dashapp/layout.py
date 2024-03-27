2f1f14a02c762c2e48a328c333558b3e094742ba
Jie01236 <151651468+Jie01236@users.noreply.github.com>
2024-03-28 00:29:38 +0100

    Update layout.py

--- dashapp/layout.py
+++ dashapp/layout.py
@@ -2,14 +2,16 @@ from dash import html, dcc
 
 def create_layout():
     layout = html.Div([
+         # Création d'un menu déroulant pour le filtre de fonction (Président ou Premier Ministre)
         dcc.Dropdown(
-            id='fonction_filter',  
+            id='fonction_filter',  # Identifiant du composant
             options=[
                 {'label': 'Président', 'value': 'president de la republique'},  
                 {'label': 'Premier Ministère', 'value': 'premier ministre'}
             ],
-            value='president de la republique'  
+            value='president de la republique'   # Valeur par défaut du menu déroulant
         ),
+        # Création d'un deuxième menu déroulant pour le filtre de localisation (domicile ou étranger)
         dcc.Dropdown(
             id='location_filter', 
             options=[
@@ -18,12 +20,14 @@ def create_layout():
             ],
             value='etrangers' 
         ),
+        # Création d'un troisième menu déroulant dépendant du choix dans le deuxième menu
         dcc.Dropdown(
             id='location_filter_2', 
-            options=[],
-            placeholder="Choisir un pays/une ville",  
-            value=None,
+            options=[], # Les options seront mises à jour dynamiquement en fonction de la sélection dans le menu précédent
+            placeholder="Choisir un pays/une ville",  # Texte affiché lorsqu'aucune option n'est sélectionnée
+            value=None, # Aucune valeur sélectionnée par défaut
         ),
+         # Création d'un composant pour afficher des graphiques
         dcc.Graph(id='travel-graph')  
     ])
-    return layout
+    return layout # Retourne la mise en page définie
